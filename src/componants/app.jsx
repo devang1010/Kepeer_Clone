42b3a24ade8e76dc4b25e407fdc3b64789191ea9
devang1010 <devangt101003@gmail.com>
2023-09-11 13:46:03 +0530

    part-2 completed

--- src/componants/app.jsx
+++ src/componants/app.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <Note />
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      {notes.map( (noteItem) => {
+        return (
+          <Note
+            key={noteItem.id}
+            title={noteItem.title}
+            content={noteItem.content}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
