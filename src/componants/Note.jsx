f5ae4082cd041337ff2e7640d3f9df0a520622b6
devang1010 <devangt101003@gmail.com>
2023-09-20 13:33:44 +0530

    nothing

--- src/componants/Note.jsx
+++ src/componants/Note.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-function Note(props){
+export default function Note(props){
     return (<div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
     </div>)
 }
 
-export default Note;
